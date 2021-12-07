08ed388c6fb850085a3b4c817c4903c0a90d89ea
James Bell <jbell@OdenHughes.local>
2021-12-07 11:57:27 -0600

    Adding app files

---
+++ config.py
@@ -0,0 +1,11 @@
+client_id = "43d7883d1762fdb86482b05ff9f4e2ec86a70ee64dc9275680dbc789ccab7f50"
+
+client_secret = "2686c77e697c24f3eb859e0929b876495991d4d82c3e917a8801021b559e28c0"
+
+redirect_url = "http://localhost:3000/users/home"
+
+base_url = "https://api.procore.com"
+
+oauth_url = "https://login.procore.com/"
+
+db_pw = "JBoht2021!"
